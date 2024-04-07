98d24f0caa4bbbc4ab9fafb00c60951f653636ca
Kajirobachi <nuber-jonas@web.de>
2024-04-07 22:01:19 +0200

    Appended User Management Sub-Domain

--- Core/UserManagement/src/main/java/org/nuberjonas/sentrycube/core/usermanagement/domain/valueobjects/ProfilePicture.java
+++ Core/UserManagement/src/main/java/org/nuberjonas/sentrycube/core/usermanagement/domain/valueobjects/ProfilePicture.java
@@ -2,5 +2,17 @@ package org.nuberjonas.sentrycube.core.usermanagement.domain.valueobjects;
 
 public final class ProfilePicture {
 
-    private int image;
+    private final byte[] data;
+
+    public ProfilePicture(byte[] data) {
+        this.data = data;
+    }
+
+    public ProfilePicture change(byte[] newData){
+        return new ProfilePicture(newData);
+    }
+
+    public byte[] get(){
+        return data;
+    }
 }
