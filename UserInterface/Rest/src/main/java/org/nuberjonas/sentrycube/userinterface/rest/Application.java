c45707b45b66359cd241293d1e613d1d2fd8cd62
Kajirobachi <nuber-jonas@web.de>
2024-05-21 08:56:25 +0200

    Fixed loading of infrastructure
    Okay... I need to rent somewhere. I cannot express how much time I've spent on trying to get this to work and the @EntityScan fixes it.... I want to cry

--- UserInterface/Rest/src/main/java/org/nuberjonas/sentrycube/userinterface/rest/Application.java
+++ UserInterface/Rest/src/main/java/org/nuberjonas/sentrycube/userinterface/rest/Application.java
@@ -2,10 +2,12 @@ package org.nuberjonas.sentrycube.userinterface.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"org.nuberjonas.sentrycube.userinterface.rest", "org.nuberjonas.sentrycube.infrastructure.persistence.jpa"})
-@EnableJpaRepositories(basePackages = {"org.nuberjonas.sentrycube.infrastructure.persistence.jpa"})
+@EnableJpaRepositories(basePackages = {"org.nuberjonas.sentrycube.infrastructure.persistence.jpa.repositories"})
+@EntityScan(basePackages = {"org.nuberjonas.sentrycube.infrastructure.persistence.jpa.tables", "org.nuberjonas.sentrycube.infrastructure.persistence.jpa.data"})
 public class Application {
 
     public static void main(String[] args) {
